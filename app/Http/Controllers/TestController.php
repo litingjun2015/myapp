0a7998a36cb3f371568e43df01bff347e98a734d
John <tingjun.li@qq.com>
2019-06-22 19:12:56 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,10 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+require __DIR__ . '/../../../vendor/autoload.php';
+
+# Imports the Google Cloud client library
+use Google\Cloud\Translate\TranslateClient;
+
+# Your Google Cloud Platform project ID
+
+
 class TestController extends Controller
 {
     public function index()
     {
-        return "you can do test here.";
+        // putenv('GOOGLE_APPLICATION_CREDENTIALS='.resource_path().'/google.credentials.json');
+        
+        // $projectId = 'starlit-granite-20190622';
+
+        // # Instantiates a client
+        // $translate = new TranslateClient([
+        //     'projectId' => $projectId
+        // ]);
+
+        // # The text to translate
+        // $text = 'Hello, world!';
+        // # The target language
+        // $target = 'ru';
+
+        // # Translates some text into Russian
+        // $translation = $translate->translate($text, [
+        //     'target' => $target
+        // ]);
+
+        // echo 'Text: ' . $text . '
+        // Translation: ' . $translation['text'];
+
+        
+
+
+        return  'Translation is coming';
     }
 }
--- tutorials.md
+++ tutorials.md
@@ -3,6 +3,9 @@
 #
 
 ##
+export GOOGLE_APPLICATION_CREDENTIALS="/home/vagrant/Code/myapp/config/starlit-granite-20190622-118100326d54.json"
+
+##
 php artisan make:controller TestController
 
 
