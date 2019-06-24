5d3f7039e430e78ff6c3ac315eefa0cf53424be0
John Li <tingjun.li@gmail.com>
2019-06-24 15:16:40 +0800

    AMR

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -101,7 +101,7 @@ class WechatController extends Controller
 
                     # The audio file's encoding, sample rate and language
                     $config = new RecognitionConfig([
-                        'encoding' => AudioEncoding::LINEAR16,
+                        'encoding' => AudioEncoding::AMR,
                         'sample_rate_hertz' => 8000,
                         'language_code' => 'en-US'
                     ]);
