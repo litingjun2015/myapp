137eea5ff409c8671a5d7938b44cad16cb699e03
John Li <tingjun.li@gmail.com>
2019-06-24 14:05:31 +0800

    test

--- public/translate/speech2text.php
+++ public/translate/speech2text.php
@@ -8,7 +8,7 @@ use Google\Cloud\Speech\V1\RecognitionAudio;
 use Google\Cloud\Speech\V1\RecognitionConfig;
 use Google\Cloud\Speech\V1\RecognitionConfig\AudioEncoding;
 
-putenv('GOOGLE_APPLICATION_CREDENTIALS='.resource_path().'/google.credentials.json');
+putenv('GOOGLE_APPLICATION_CREDENTIALS='.'/home/forge/default/public/translate/google.credentials.json');
 
 # The name of the audio file to transcribe
 $audioFile = __DIR__ . '/audio32KHz.raw';
