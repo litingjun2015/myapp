73be19038499459212997445215b8c2862261dac
John <tingjun.li@qq.com>
2019-06-24 19:37:52 +0800

    tt

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -207,127 +207,13 @@ class TestController extends Controller
             //TODO 发送语音
             
             return $result;
+        })->push(function ($message) {
+            return "您好！欢迎关注我!";
         });
 
-        $app->server->push(function ($message) {
-
-            //TODO
-            putenv('GOOGLE_APPLICATION_CREDENTIALS='.resource_path().'/google.credentials.json');
         
-            # Your Google Cloud Platform project ID
-            $projectId = 'starlit-granite-20190622';
-
-            # Instantiates a client
-            $translate = new TranslateClient([
-                'projectId' => $projectId
-            ]);
-
-            switch ($message['MsgType']) {
-                case 'event':
-                    break;
-                case 'text':
-                    \Log::debug('收到文字消息');
-                    break;
-                case 'image':
-                    break;
-                case 'voice':
-                    \Log::debug('收到语音消息');  
-
-                    $date = date("Ymdhms");
-                    list($usec, $sec) = explode(" ", microtime());  
-                    $msec=round($usec*1000);  
-                    $millisecond = str_pad($msec,3,'0',STR_PAD_RIGHT);
-                    $timestring = $date.$millisecond;
-
-                    \Log::debug("audio".$timestring.".raw");
-                    \Log::debug($message['MediaId']);
-
-                    $stream = $app->media->get($message['MediaId']);
-
-                    if ($stream instanceof \EasyWeChat\Kernel\Http\StreamResponse) {
-                        // // 以内容 md5 为文件名存到本地
-                        // $stream->save('保存目录');
-
-                        // 自定义文件名，不需要带后缀
-                        $stream->saveAs('/tmp/', "audio".$timestring.".raw");
-                    }
-
-                    break;
-                case 'video':                
-                    break;
-                case 'location':
-                    break;
-                case 'link':
-                    break;
-                // ... 其它消息
-                default:
-                    break;
-            }
-
-
-        
-
-
-            # The text to translate
-            $text = $message['Content'];
-
-            if($text === '汇率') {
-                $result = sprintf("%-48s%-40s%-66s%-46s%-40s%-66s%-46s%-40s", 
-                "1美元=6.8698人民币", "1人民币=0.1456美元", "", "1越南盾=0.00030人民币", "1人民币=3364.11越南盾", "","1美元=23137.75越南盾", "1越南盾=0.000043美元" );
-            } else {
-
-                # 语言检测
-                $detectResult = $translate->detectLanguage($text);
-                \Log::debug("Language code: $detectResult[languageCode]\n");
-                \Log::debug("Confidence: $detectResult[confidence]\n");
-
-                if($detectResult['languageCode'] === 'zh-CN'){
-                    # The target language
-                    $target = 'vi';
-
-                    # Translates some text into Russian
-                    $translation = $translate->translate($text, [
-                        'target' => $target
-                    ]);
-                    \Log::debug($translation);
-
-                    $result = '【'.$message['Content'].'】 所对应越南语的意思是：'.$translation['text'];
-                }else if($detectResult['languageCode'] === 'vi'){
-                     # The target language
-                     $target = 'zh-CN';
-
-                     # Translates some text into Russian
-                     $translation = $translate->translate($text, [
-                         'target' => $target
-                     ]);
-                     \Log::debug($translation);                    
- 
-                     $result = '【'.$message['Content'].'】 所对应中文的意思是：'.$translation['text'];
-                }
-                
-
-            }
-            
-            //TODO 发送语音
-            
-            return $result;
-        });
 
         $response = $app->server->serve();     
-        \Log::debug($response);
-
-
-        $app->server->push(function ($message) {
-            return "您好！欢迎关注我!";
-        });
-        
-        $response = $server->serve();
-
-        $app->server->push(function ($message) {
-            return "您好！欢迎关注我!";
-        });
-        
-        $response = $server->serve();
 
         return $response;
     }
