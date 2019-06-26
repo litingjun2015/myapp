a5b24a2bc858b1ee73e9ef629fc139af45f6fac3
John Li <tingjun.li@gmail.com>
2019-06-26 13:29:31 +0800

    refactor(wechat csrf)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,8 +33,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            //TODO https://www.easywechat.com/discussions/28
-            // \App\Http\Middleware\VerifyCsrfToken::class,
+             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
